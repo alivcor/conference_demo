67eb9efedfc871eaff0a917ef2871b8e3a788d46
alivcor <abhinandandubey@live.com>
2020-08-17 18:14:50 -0400

    Setter Injection

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,3 +1,5 @@
+import com.iresium.repository.HibernateSpeakerRepositoryImpl;
+import com.iresium.repository.SpeakerRepository;
 import com.iresium.service.SpeakerService;
 import com.iresium.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,14 @@ public class AppConfig {
 
     @Bean(name = "speakerService")
     public SpeakerService getSpeakerService() {
-        return new SpeakerServiceImpl();
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service
+    }
+
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
     }
 }
